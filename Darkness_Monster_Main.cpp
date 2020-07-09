573fe06f34a20bbfa6b6757cb7e93b8b74dfa9f0
jmee1023 <58606343+jmee1023@users.noreply.github.com>
2020-07-09 18:02:17 -0400

    Update Darkness_Monster_Main.cpp

--- Darkness_Monster_Main.cpp
+++ Darkness_Monster_Main.cpp
@@ -20,14 +20,13 @@ int get_int_input(string prompt, string invalid, int low, int high) {
 int main() {
 
     /*
-      TODO: Create a vector of Darkness Monster unique pointers and push a variety of ten Closet Monsters and Under Bed Monsters to it.
-     ODO: Tell the user that there are monsters everywhere!
-     * TODO: Loop through the vector and call the scare method on each. Print the return values.
+      Create a vector of Darkness Monster unique pointers and push a variety of ten Closet Monsters and Under Bed Monsters to it.
+      Tell the user that there are monsters everywhere!
+     *  Loop through the vector and call the scare method on each. Print the return values.
      *     - This is polymorphism. Why? Answer in your README file.
-     * TODO: Repeatedly give the user options to poke the eyes or run away.
+         Repeatedly give the user options to poke the eyes or run away.
      *     - If they poke the eyes, loop through the vector and call the poke_eye method on each.
-     *         - This is NOT polymorphism. Why? Answer in your README file.
-     *         - After poking all the eyes, loop through the vector again and output the scare methods again.
+     *    
      *     - If they choose to run away, the program ends.
      */
     vector<unique_ptr<Darkness_Monster>> darkMonsters;
